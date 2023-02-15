fa9f4537f00c344e848ae90a44dc55a64d5a9b1c
isayaksh <isayaksh@gmail.com>
2023-02-15 14:12:10 +0900

    [Chapter07] 클래스

--- Java/src/chapter_08/_00_AccessModifierTest.java
+++ Java/src/chapter_08/_00_AccessModifierTest.java
@@ -1,2 +1,16 @@
-package chapter_08;public class _00_AccessModifierTest {
+package chapter_08;
+
+import chapter_07.BlackBoxRefurbish;
+
+public class _00_AccessModifierTest {
+    public static void main(String[] args) {
+        BlackBoxRefurbish b1 = new BlackBoxRefurbish();
+        b1.modelName = "까망이";
+//        b1.resolution = "FHD";
+//        b1.price = 200000;
+//        b1.color = "블랙";
+
+
+
+    }
 }
